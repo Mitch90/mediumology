c8031e90048cf05ba86a28ecb942419e529d3915
Michele Invernizzi <michele.invernizzi90@gmail.com>
2016-12-06 13:22:54 +0100

    New version of mediumology: replace nightmare with request

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mediumology
-This tool will help you create a network of related tags on Medium, starting from a given topic. The output are two tsv files (one for the nodes and one for the edges) that can be later used in softwares such as Gephi. 
+This tool will help you create a network of related tags on Medium, starting from a given topic. The output are two tsv files (one for the nodes and one for the edges) that can be later used in softwares such as Gephi.
 
 ###To run mediumology:
 
@@ -7,23 +7,23 @@ This tool will help you create a network of related tags on Medium, starting fro
 
 * Clone the repository
 
-* Go to the mediumology folder 
+* Go to the mediumology folder
 ```
 cd pathToTheMediumologyFolder
 ```
 
-* Install node modules 
+* Install node modules
 ```
 npm install
 ```
 
-* Open **index.js** and choose the topic to analyze and the depth of the search 
-```javascript 
-var startingPoint = 'privacy', 
-    depth = 3;
+* Open **index.js** and choose the topic to analyze and the depth of the search (it must be an existing tag on Medium obviously)
+```javascript
+var initialTopic = 'Privacy',
+    depth = 2;
 ```
 
-* Run the script 
+* Run the script
 ```
 node index.js
 ```
--- index.js
+++ index.js
@@ -1,90 +1,103 @@
-//set starting topic to investigate and search depth
-var startingPoint = 'privacy',
-    depth = 3;
+//set up the startingpoint and depth of the crawl
+var initialTopic = 'Privacy',
+    depth = 2;
 
-//import dependencies
-var fs = require('fs'),
-    _ = require('underscore'),
-    Nightmare = require('nightmare'),
-    nightmare = Nightmare({
-        show: true
-    });
+//get dependencies
+var request = require('request'),
+    fs = require('fs'),
+    _ = require('underscore');
 
 //set dictionary, counters variables, tsv headers
 var topics = [],
+    startingPoint = initialTopic.toLowerCase().replace(' ', '-'),
     degreeCounter = 1,
-    indexCounter = 0,
+    indexCounter = -1,
+    initialTopicInfo = false,
     edgesHeader = 'source\ttarget\n',
-    nodesHeader = 'id\ttagDegree\n';
+    nodesHeader = 'id\ttagDegree\tfollowerCount\tpostCount\n';
 
-//start creating dictionary
-topics.push({topic: startingPoint, degree: 0});
+//get date and create new folder
+var d = new Date(),
+    date = d.toISOString(),
+    newFolder = startingPoint + "_" + date.substring(0, 10) + '(' + date.substring(11, 13) + '-' + date.substring(14, 16) + '-' + date.substring(17, 19) + ')';
+
+fs.mkdirSync(newFolder);
+
 //create edges tsv file
-fs.writeFileSync(startingPoint + '_edges.tsv', edgesHeader);
-//create nodes tsv file and append the starting node
-fs.writeFileSync(startingPoint + '_nodes.tsv', nodesHeader);
-fs.appendFileSync(startingPoint + '_nodes.tsv', topics[0].topic + '\t' + topics[0].degree + '\n');
+fs.writeFileSync(newFolder + '/' + startingPoint + '_edges.tsv', edgesHeader);
+//create nodes tsv file
+fs.writeFileSync(newFolder + '/' + startingPoint + '_nodes.tsv', nodesHeader);
 
-//scrape related tags from Medium pages
-function getTagsFromPages (newTopic) {
-    var newPage = newTopic.replace(' ', '-');
-    console.log('going to https://medium.com/tag/' + newPage + ", degree: " + topics[indexCounter].degree);
+//call the API
+function callAPI(tag) {
+    var currentTopic = tag.topic;
+    console.log('retrieving ' + tag.topic + '\'s related tags…');
 
-    nightmare
-        .goto('https://medium.com/tag/' + newPage)
-        .wait('.tags')
-        .evaluate(function () {
-            //get all the related tags
-            var nodeList = document.querySelectorAll('.tags .link'),
-                relatedList = [];
+    //make API call
+    request('https://medium.com/_/api/tags/' + tag.slug + '/related', function(error, response, body) {
+        console.log('Status Code: ' + response.statusCode);
+        if (!error && response.statusCode == 200) {
+            //get the JSON from page
+            var re = /^.*x>/,
+                cleanUp = JSON.parse(body.replace(re, ''));
 
-            nodeList.forEach(function (node) {
-                relatedList.push(node.innerText.toLowerCase());
+            var tagJson = cleanUp.payload.relatedTags;
+            console.log('Got ' + tagJson.length + ' tags.');
+
+            _.each(tagJson, function(tagObject) {
+                //check if the topic is already in the dictionary
+                var match = _.findIndex(topics, {
+                    topic: tagObject.name
+                });
+                if (match === -1) {
+                    //check if the tag is the startingPoint (the first time it happens)
+                    if (tagObject.slug === 'privacy') {
+                        //make sure the starting node is added only once
+                        if (!initialTopicInfo) {
+                            //update the node tsv
+                            fs.appendFileSync(newFolder + '/' + startingPoint + '_nodes.tsv', tagObject.name + '\t' + 0 + '\t' + tagObject.metadata.followerCount + '\t' + tagObject.postCount + '\n');
+                            initialTopicInfo = !initialTopicInfo;
+                        }
+                    } else {
+                        //if the topic wasn't present n the dictionary, add it
+                        topics.push({
+                            topic: tagObject.name,
+                            degree: degreeCounter,
+                            followerCount: tagObject.metadata.followerCount,
+                            postCount: tagObject.postCount,
+                            slug: tagObject.slug
+                        });
+                        console.log('New topic found! Added ' + tagObject.name + ' to dictionary.');
+                        //update the node tsv
+                        fs.appendFileSync(newFolder + '/' + startingPoint + '_nodes.tsv', _.last(topics).topic + '\t' + _.last(topics).degree + '\t' + _.last(topics).followerCount + '\t' + _.last(topics).postCount + '\n');
+                    }
+                }
+                //update edges tsv with new found connections
+                var newEdgeLine = currentTopic + '\t' + tagObject.name + '\n';
+                fs.appendFileSync(newFolder + '/' + startingPoint + '_edges.tsv', newEdgeLine);
             });
 
-            return relatedList;
-        })
-        .then(function (list) {
-            addLinesToTsv(list);
-        });
-};
+            console.log('All ' + tagJson.length + ' tags scraped.');
+            //update counter to get the right node from the topics' list
+            indexCounter++;
+            //update counter to give the right degrees to every node
+            if (topics[indexCounter].degree === degreeCounter) {
+                degreeCounter++;
+            }
+            //check if the search is going beyond the predefined depth
+            if (degreeCounter < depth + 1) {
+                console.log('\nMoving on…');
+                //call API again with new tag
+                callAPI({topic: topics[indexCounter].topic, slug: topics[indexCounter].slug});
+            } else {
+                console.log('\nDone! All the tags related to ' + initialTopic + ' have been scraped, until depth ' + depth);
+            }
 
-//the function adds all the connections found to the edges and nodes tsv
-function addLinesToTsv (list) {
-    console.log('scraping tags…');
-    list.forEach(function (element) {
-        //check if the topic is already in the dictionary
-        var match = _.findIndex(topics, {topic: element});
-        if (match === -1) {
-            //if not add it and update the nodes tsv
-            topics.push({topic: element, degree: degreeCounter});
-            fs.appendFileSync(startingPoint + '_nodes.tsv', _.last(topics).topic + '\t' + _.last(topics).degree + '\n');
         }
-        //update edges tsv with new found connections
-        var newEdgeLine = topics[indexCounter].topic + '\t' + element + '\n';
-        fs.appendFileSync(startingPoint + '_edges.tsv', newEdgeLine);
     });
-    console.log(topics[indexCounter].topic + '\'s related tags scraped.');
-    //update counter to get the right node from the topics' list
-    indexCounter++;
-    //update counter to give the right degrees to every node
-    if (topics[indexCounter].degree === degreeCounter) {
-        degreeCounter++;
-    }
-    //check if the search is going beyond the predefined depth
-    if (degreeCounter <= depth + 1) {
-        //go to new Medium page
-        getTagsFromPages(topics[indexCounter].topic);
-    } else {
-        //close Electron
-        nightmare
-            .evaluate()
-            .end()
-            .then();
+}
 
-        console.log('Done!');
-    }
-};
-
-//start scraping
-getTagsFromPages(startingPoint);
+callAPI({
+    topic: initialTopic,
+    slug: startingPoint
+});
